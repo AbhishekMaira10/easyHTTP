aac6931aeb1d9854679fefe2319b5d8395675e95
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-10-29 18:08:49 +0530

    writing get method

---
+++ easyhttp.js
@@ -0,0 +1,22 @@
+function easyHTTP() {
+  this.http = new XMLHttpRequest();
+}
+// Make an HTTP GET Request
+easyHTTP.prototype.get = function (url) {
+  this.http.open("GET", url, true);
+
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      console.log(self.http.responseText);
+    }
+  };
+
+  this.http.send();
+};
+
+// Make an HTTP POST Request
+
+// Make an HTTP PUT Request
+
+// Make an HTTP DELETE Request
