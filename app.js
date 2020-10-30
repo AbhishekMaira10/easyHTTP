2fb8186a0db1916781b652d815048ded7c72a5df
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-10-30 13:37:04 +0530

    update get method with callback function

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const http = new easyHTTP;
+const http = new easyHTTP();
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts')
+http.get("https://jsonplaceholder.typicode.com/posts", function (err, posts) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+});
--- easyhttp.js
+++ easyhttp.js
@@ -2,13 +2,15 @@ function easyHTTP() {
   this.http = new XMLHttpRequest();
 }
 // Make an HTTP GET Request
-easyHTTP.prototype.get = function (url) {
+easyHTTP.prototype.get = function (url, callback) {
   this.http.open("GET", url, true);
 
   let self = this;
   this.http.onload = function () {
     if (self.http.status === 200) {
-      console.log(self.http.responseText);
+      callback(null, self.http.responseText);
+    } else {
+      callback("Error: " + self.http.status);
     }
   };
 
