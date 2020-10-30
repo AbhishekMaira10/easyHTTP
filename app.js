f1f9c0a204cbec6400a52497d93ef5d10420d33b
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-10-30 17:17:37 +0530

    add delete method

--- app.js
+++ app.js
@@ -26,13 +26,25 @@ const data = {
 // });
 
 // Update ddta
-http.put("https://jsonplaceholder.typicode.com/posts/1", data, function (
+// http.put("https://jsonplaceholder.typicode.com/posts/1", data, function (
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete Post
+http.delete("https://jsonplaceholder.typicode.com/posts/1", function (
   err,
-  post
+  response
 ) {
   if (err) {
     console.log(err);
   } else {
-    console.log(post);
+    console.log(response);
   }
 });
--- easyhttp.js
+++ easyhttp.js
@@ -43,7 +43,20 @@ easyHTTP.prototype.put = function (url, data, callback) {
   this.http.send(JSON.stringify(data));
 };
 // Make an HTTP DELETE Request
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open("DELETE", url, true);
 
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, 'Post Deleted');
+    } else {
+      callback("Error: " + self.http.status);
+    }
+  };
+
+  this.http.send();
+};
 
 
 
