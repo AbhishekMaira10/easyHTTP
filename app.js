b2cc8085cd7b4a82ae2eb81bfeba8bd3b2fc6f16
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-10-30 16:34:12 +0530

    add put request

--- app.js
+++ app.js
@@ -16,8 +16,20 @@ const data = {
 };
 
 // Create Post
-http.post('https://jsonplaceholder.typicode.com/posts',data, function(err, post){
+// http.post('https://jsonplaceholder.typicode.com/posts',data, function(err, post){
 
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update ddta
+http.put("https://jsonplaceholder.typicode.com/posts/1", data, function (
+  err,
+  post
+) {
   if (err) {
     console.log(err);
   } else {
--- easyhttp.js
+++ easyhttp.js
@@ -31,5 +31,15 @@ easyHTTP.prototype.post = function (url, data, callback) {
 };
 
 // Make an HTTP PUT Request
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open("PUT", url, true);
+  this.http.setRequestHeader("Content-Type", "application/json");
 
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
 // Make an HTTP DELETE Request
